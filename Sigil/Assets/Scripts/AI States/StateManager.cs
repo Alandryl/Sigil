dbd6cd41bef2d25e33588d85be7bc478eb37d0ed
Christian Søndergaard Weatherley <alandryl@live.dk>
2022-12-05 22:48:49 +0100

    First Enemy AI

---
+++ Sigil/Assets/Scripts/AI States/StateManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    public State currentState;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RunStateMachine();
+    }
+
+    private void RunStateMachine()
+    {
+        State nextState = currentState?.RunCurrentState();
+
+        if (nextState != null)
+        {
+            SwitchToNextState(nextState);
+        }
+    }
+
+    private void SwitchToNextState(State nextState)
+    {
+        currentState = nextState;
+    }
+
+}
